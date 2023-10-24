53fde15cb6120111d5d9f8e15268be8d38418ee9
MrDiidouu <139962257+MrDiidouu@users.noreply.github.com>
2023-10-24 16:36:59 +0200

    Add files via upload

--- Tableau.php
+++ Tableau.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .border{
+            border: solid white;
+            text-align: center;         
+        }
+    </style>
     <title>Tableaux en php</title>
 </head>
 <body>
@@ -69,6 +75,78 @@
             echo $tb[$i],"<br>";
             $i++;
         }
+
+        for($i = 0;$i <= 10; $i++){
+            $tab3[$i] = pow(2,$i);
+        }
+        foreach($tab3 as $ind => $val){
+            echo "<br> <br> 2 puissances $ind vaut $val";
+        }
+        echo "<br> Dernier indice $ind , dernière valeur $val <br> <br>";
+        
+        $tab4 = array(
+            array("LIBRAIRIE", "TITRES", "REMISE"),
+            array("FNAC", "GOOGLE SHEETS", "40%"),
+            array("FNAC", "EXCEL", "40%"),
+            array("EXPRESS", "GOOGLE DOCS", "38"),
+            array("EXPRESS", "GOOGLE SLIDES", "40"),
+        );
+        
+        echo "<table class=\"border\" border=1 width=\"50%\">";
+        //Début du tableau
+        echo "<tr class=\"border\"><th class=\"border\">" . implode("</th><th class=\"border\">", $tab4[0]) . "</th></tr>";
+        
+        //Lecture des lignes à partir de la deuxième ligne
+        for ($i = 1; $i < count($tab4); $i++) {
+            list($librairie, $titre, $remise) = $tab4[$i];
+            echo "<tr class=\"border\">
+            <td class=\"border\">$librairie</td>
+            <td class=\"border\">$titre</td>
+            <td class=\"border\">$remise</td>
+            </tr>";
+        }
+        
+        //Fin du tableau
+        echo "</table>";
+        
+        echo "<br> <br>";
+
+        $tablo[0] = "Pomme";
+        $tablo[1] = "Poire";
+        $tablo[2] = "Orange";
+        $tablo[3] = "Cerise";
+        $tablo[4] = "Mangue";
+        //je relie les éléments en les affichant dans le browser
+        echo "<ol>"; //je prépare une liste numérotée
+        for($i = 0; $i < count($tablo);$i++){
+            echo "<li>" .$tablo[$i]. "</li>"; // J'affiche chaque élément du tableau
+        }
+        echo "</ol> <br>";
+        echo "<br>";
+        $tablo1[] = "Stylo";
+        $tablo1[] = "Crayon";
+        $tablo1[] = "Gomme";
+        $tablo1[] = "Règle";
+        $tablo1[] = "Cahier";
+        // je relie les éléments en commençant par 0
+        for($i = 0;$i < count($tablo1);$i++){
+            echo $tablo1[$i]."<br>";
+        }
+
+        echo "<br><br>";
+        $tablo2 =  ["Voiture","Vélo","Moto","Camion","Bateaux"];
+        echo "<br>";
+        print_r($tablo2);
+
+        /* 
+        En résumé 
+        Les tableaux sont des structures de variables complexes à une ou plusieur dimensions. Un indice numérique ou texte permet de récupérer la valeur d'une case d'un tableau. Dans un indice numérique, on utiliser une boucle for ou while pour lire les éléments d'un tableau.
+        Dans le cas d'un indice de type texte, il faut employer la boucle foreach.
+        Les tableaux servent aussi à organiser des matrices d'informations que l'on récupère si besoin en les parcourant à l'aide d'une ou plusieurs boucles. 
+        */
+
+
+        
 
 
 
