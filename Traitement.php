8e46c1c99131abd84c15afca91369c4f10f97bc8
MrDiidouu <139962257+MrDiidouu@users.noreply.github.com>
2023-11-15 16:18:39 +0100

    Add files via upload

---
+++ Traitement.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>traitement</title>
+</head>
+<body>
+    <?php 
+    $connexion = '192.168.107.20';
+    $user = 'root';
+    $post = '';
+    
+    ?>
+
+</body>
+</html>
