75b92e1d4e799ccf5718d91a60dc17077de8c7e2
MrDiidouu <139962257+MrDiidouu@users.noreply.github.com>
2023-11-22 12:55:46 +0100

    Add files via upload

---
+++ Portofolio.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://diidou.web.app/Mimo/Image/Diidou2.png">
+    <link rel="stylesheet" href="Sass/Portofolio.css">
+    <title>Portofolio</title>
+</head>
+
+<body>
+    <!-- Inclure le menu -->
+    <?php
+        include_once('Menu.php')
+    ?>
+    <section class="Conteneur-body">
+        <section class="h1"><h1>Voici les projets sur lequelle j'ai travailler et je travaille</h1></section>
+        <section class="Set-Card1">
+            
+            <div class="Card-1">
+                <div class="Title-Card">
+                    <h3>Premier Cv en HTML</h3>
+                </div>
+                <div class="Img-Card">
+                    <img src="image/Cv_version_1.png" alt="" width="98%">
+                </div>
+                <div class="Def-Card">
+                    <p>Ceci est mon premier CV fait en HTML, CSS, il n'est pas énormement rempli mais il ai beau</p>
+                </div>
+            </div>
+
+            <div class="Card-2">
+                <div class="Title-Card">
+                    <h3>Site En ligne</h3>
+                </div>
+                <div class="Img-Card">
+                    <img src="image/Site_Tuto.png" alt="" width="98%">
+                </div>
+                <div class="Def-Card">
+                    <p>J'ai crée ce site pour mettre les cours de l'application mimo a un endroit pour m'en souvenir et y
+                        accéder plus facilement. Or c'est devenue mon Site principale et ma bibliothéque de contenue</p>
+                </div>
+            </div>
+
+            <div class="Card-3">
+                <div class="Title-Card">
+                    <h3>Exercice Javascript</h3>
+                </div>
+                <div class="Img-Card">
+                    <img src="image/Tp_JavaScript.png" alt="">
+                </div>
+                <div class="Def-Card">
+                    <p>J'ai fait en sorte que cette exercice me servent dans casiment toute les situations</p>
+                </div>
+            </div>
+
+
+            <div class="Card-4">
+                <div class="Title-Card">
+                    <h3>Site Vitrine</h3>
+                </div>
+                <div class="Img-Card">
+                    <img src="image/Site_vitrine1.png" alt="">
+                </div>
+                <div class="Def-Card">
+                    <p>Petit site vitrine basique avec un pied de page avec contact et les information.
+                    </p>
+                </div>
+            </div> 
+        </section>
+        <section class="Set-Card2">
+            <div class="Card-5">
+                <div class="Title-Card"><h3>Magic</h3></div>
+                <div class="Img-Card"><img src="image/" alt=""></div>
+                <div class="Def-Card"><p>Crédit</p></div>
+            </div>
+            <div class="Card-6">
+                <div class="Title-Card"><h3>Magic</h3></div>
+                <div class="Img-Card"><img src="image/" alt=""></div>
+                <div class="Def-Card"><p>Crédit</p></div>
+            </div>
+            <div class="Card-7">
+                <div class="Title-Card"><h3>Magic</h3></div>
+                <div class="Img-Card"><img src="image/" alt=""></div>
+                <div class="Def-Card"><p>Crédit</p></div>
+            </div>
+            <div class="Card-8">
+                <div class="Title-Card"><h3>Formulaire de calcul</h3></div>
+                <div class="Img-Card"><img src="image/Calcule_Formulaire1.png" alt=""></div>
+                <div class="Def-Card"><p>Ce formulaire à été fais a la base pour un exercice pour ma formation mais je l'ai pousser pour qu'il sois vraiment utilisable</p></div>
+            </div>
+        
+        </section>
+
+    </section>
+
+
+
+</body>
+
+</html>
