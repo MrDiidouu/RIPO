f999c1082dbd348a81dcd1609cf69780f140a4e9
MrDiidouu <139962257+MrDiidouu@users.noreply.github.com>
2023-12-04 16:40:10 +0100

    Add files via upload

---
+++ Compétences.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://diidou.web.app/Mimo/Image/Diidou2.png">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="Sass/Compétences.css">
+    <title>Compétences</title>
+</head>
+<body>
+    <!-- Inclure le menu -->
+    <?php 
+        include_once('Menu.php');
+    ?>
+    <!-- HTML -->
+    <h2>Compétences</h2>
+    <div class="competences">
+        <div class="barre-competence" id="HTML5">HTML5</div>
+        <div class="barre-competence" id="CSS3">CSS3</div>
+        <div class="barre-competence" id="JavaScript">JavaScript</div>
+        <div class="barre-competence" id="VS-Code">Visual Studio Code</div>
+        <div class="barre-competence" id="PHP">PHP</div>
+        <div class="barre-competence" id="MySQL">MySQL</div>
+        <div class="barre-competence" id="Github">Github</div>
+    </div> 
+
+  
+    <script src="Script/Script.js"></script>
+</body>
+</html>
