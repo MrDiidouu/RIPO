cbbbbbee8769b90a7a88b8c808ea514278dc5517
MrDiidouu <139962257+MrDiidouu@users.noreply.github.com>
2024-01-09 16:35:11 +0100

    Add files via upload

--- user.sql
+++ user.sql
@@ -1,15 +1,38 @@
-:/*
-MYsql et un sysyteme de gestion de bases de données relationnelles populaire utulisé pour stocker et recuper edes données. la gestion des utulisateuur dans MYsql est cruciale pour garantir et limiter l'aces aux données sensible.
 
-Structure des utulisateurs
+/*
+MySQL est un système de gestion de bases de données relationnelles populaire utilisé pour stocker et récupérer des données. La gestion des utilisateurs dans MySQL est cruciale pour garantir et limiter l'accès aux données sensibles.
 
-chaque utulisateur est identifié par un nom (uername) et associé a un hote (host) depuis lequel il peut se connecter.
+Structure des utilisateurs:
 
-La combinaison de l'utulisateur et de l'hote est unique.ADD
-
-Gestion des utullisateurs
-1. Création des utulisateurs
-
-Pour créer un utilisateur, on utulise la commande
+Chaque utilisateur est identifié par un nom d'utilisateur (username) et associé à un hôte (host) depuis lequel il peut se connecter. La combinaison de l'utilisateur et de l'hôte est unique.
 */
-create user 'nomutilisateur'@'hote' IDENTIFIED BY 'mot de passe';
+-- Création d'un utilisateur
+CREATE USER 'nomutilisateur'@'hote' IDENTIFIED BY 'motdepasse';
+
+-- Modifier le mot de passe d'un utilisateur
+ALTER USER 'johnny'@'localhost' IDENTIFIED BY 'nouveaumotdepasse';
+
+-- Renommer un utilisateur
+RENAME USER 'johnny'@'localhost' TO 'tommy'@'localhost';
+
+-- Supprimer un utilisateur
+DROP USER 'tommy'@'localhost';
+
+-- Voir la liste des utilisateurs
+SELECT user FROM mysql.user;
+
+-- Voir l'utilisateur courant
+SELECT current_user;
+
+-- Gestion des PRIVILEGES
+-- Accorder des privilèges à un utilisateur sur une base de données
+GRANT privilege ON database.* TO 'user'@'host';
+
+GRANT ALL ON bdd.* to 'user'@'hote'; /* hote = localhost */
+
+-- Révoquer des privilèges à un utilisateur sur une base de données
+REVOKE privilege ON database.* FROM 'user'@'host';
+-
+
+
+
